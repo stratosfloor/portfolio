4de2995cfee6ad23aad004f97acea1481303a0e6
Emil Noren <stratosfloor@hotmail.com>
2023-10-09 14:51:48 +0200

    Change margin bottom and add section divier

--- components/about.tsx
+++ components/about.tsx
@@ -6,7 +6,7 @@ import { motion } from 'framer-motion';
 export default function About() {
 	return (
 		<motion.section
-			className="mb-28 max-w-[45rem] text-center leading-8 sb:mb-40"
+			className="max-w-[45rem] text-center leading-8 mb:28 sm:mb-0"
 			initial={{ opacity: 0, y: 100 }}
 			animate={{ opacity: 1, y: 0 }}
 			transition={{ delay: 0.175 }}
