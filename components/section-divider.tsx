8cf5ed28220b7fe7e6c316694c34feb83aa6442a
Emil Noren <stratosfloor@hotmail.com>
2023-10-09 14:49:47 +0200

    Change to take margin y as string parameter

--- components/section-divider.tsx
+++ components/section-divider.tsx
@@ -2,11 +2,14 @@
 import { motion } from 'framer-motion';
 import React from 'react';
 
-export default function SectionDivider() {
+type SectionDividerProps = {
+	my?: string;
+};
+
+export default function SectionDivider({ my = '10' }: SectionDividerProps) {
 	return (
 		<motion.div
-			className="bg-gray-200 my-20 h-16 w-1 rounded-full hidden sm:block
-    "
+			className={`bg-gray-200 my-${my} h-16 w-1 rounded-full hidden sm:block`}
 			initial={{ opacity: 0, y: 100 }}
 			animate={{ opacity: 1, y: 0 }}
 			transition={{ delay: 0.15 }}
