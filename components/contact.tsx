1fb006e66d23f88e1ba68f97a7f0738aa6c62806
Emil Noren <stratosfloor@hotmail.com>
2023-10-10 17:21:01 +0200

    Add spinner when sending email

--- components/contact.tsx
+++ components/contact.tsx
@@ -2,10 +2,10 @@
 
 import React from 'react';
 import SectionHeading from './section-heading';
-import { FaPaperPlane } from 'react-icons/fa';
 import { motion } from 'framer-motion';
 import { useSectionInView } from '@/lib/hooks';
 import { sendEmail } from '@/actions/sendEmail';
+import SubmitBtn from './submit-btn';
 
 export default function Contact() {
 	const { ref } = useSectionInView('Contact', 0.5);
@@ -57,13 +57,7 @@ export default function Contact() {
 					required
 					maxLength={5000}
 				/>
-				<button
-					type="submit"
-					className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 active:scale-105 hover:bg-gray-950"
-				>
-					Submit{' '}
-					<FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1 " />
-				</button>
+				<SubmitBtn />
 			</form>
 		</motion.section>
 	);
