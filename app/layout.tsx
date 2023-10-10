6d5013b8fbe5e7f36042ea11fe2b7670cf6c6593
Emil Noren <stratosfloor@hotmail.com>
2023-10-10 20:38:44 +0200

    Set darkmode manually for tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 	return (
 		<html lang="en" className="!scroll-smooth">
 			<body
-				className={`${inter.className} bg-gray-100 text-gray-950 relative pt-28 sm:pt-36`}
+				className={`${inter.className} bg-gray-100 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
 			>
 				<div
 					className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem]
--- components/theme-toogle.tsx
+++ components/theme-toogle.tsx
@@ -12,9 +12,11 @@ export default function ThemeToggle() {
 		if (theme === 'light') {
 			setTheme('dark');
 			window.localStorage.setItem('theme', 'dark');
+			document.documentElement.classList.add('dark');
 		} else {
 			setTheme('light');
 			window.localStorage.setItem('theme', 'light');
+			document.documentElement.classList.remove('dark');
 		}
 	};
 
@@ -22,8 +24,12 @@ export default function ThemeToggle() {
 		const localTheme = window.localStorage.getItem('theme') as Theme | null;
 		if (localTheme) {
 			setTheme(localTheme);
+			if (localTheme === 'dark') {
+				document.documentElement.classList.add('dark');
+			}
 		} else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
 			setTheme('dark');
+			document.documentElement.classList.add('dark');
 		}
 	}, []);
 
