823a35c99863d76ce6710a82e23fcfbcdc49eff6
Emil Noren <stratosfloor@hotmail.com>
2023-10-13 21:21:51 +0200

    Add link to project

--- components/project.tsx
+++ components/project.tsx
@@ -3,12 +3,15 @@ import { useRef } from 'react';
 import { projectsData } from '@/lib/data';
 import Image from 'next/image';
 import { motion, useScroll, useTransform } from 'framer-motion';
+import { FaGithub, FaGithubSquare } from 'react-icons/fa';
+import { BiLinkExternal } from 'react-icons/bi';
 
 export default function Project({
 	title,
 	description,
 	tags,
 	imageUrl,
+	link,
 }: (typeof projectsData)[number]) {
 	const ref = useRef<HTMLDivElement>(null);
 	const { scrollYProgress } = useScroll({
@@ -29,7 +32,12 @@ export default function Project({
 		>
 			<section className="flex flex-col bg-gray-100 max-w-[48rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem]  hover:bg-gray-200 transition group-even:pl-8  dark:bg-white/10 dark:hover:bg-white/20">
 				<div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[60%] block sm:flex sm:flex-col h-full sm:group-even:ml-[18rem] ">
-					<h3 className="text-2xl">{title}</h3>
+					<h3 className="text-2xl flex items-baseline justify-">
+						{title}
+						<a href={link} target="_blank">
+							<BiLinkExternal />
+						</a>
+					</h3>
 					<p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
 						{description}
 					</p>
