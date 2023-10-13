8557807d5c4b7f396ad9a84f4082088be7a929b3
Emil Noren <stratosfloor@hotmail.com>
2023-10-13 21:36:43 +0200

    update about section

--- components/about.tsx
+++ components/about.tsx
@@ -18,29 +18,29 @@ export default function About() {
 		>
 			<SectionHeading>About me</SectionHeading>
 			<p className="mb-3">
-				After graduating with a degree in{' '}
-				<span className="font-medium">Accounting</span>, I decided to pursue my
-				passion for programming. I enrolled in a coding bootcamp and learned{' '}
-				<span className="font-medium">full-stack web development</span>.{' '}
+				After working in warehouse for a couple of years I decided to pursue my
+				passion for programming. I enrolled in a 2-year
+				<span className="font-bold"> Vocational Education</span> and learned{' '}
+				<span className="font-medium">full-stack development</span>.{' '}
 				<span className="italic">My favorite part of programming</span> is the
 				problem-solving aspect. I <span className="underline">love</span> the
 				feeling of finally figuring out a solution to a problem. My core stack
 				is{' '}
 				<span className="font-medium">
-					React, Next.js, Node.js, and MongoDB.
-				</span>
-				I am also familiar with TypeScript and Prisma. I am always looking to
-				learn new technologies. I am currently looking for a{' '}
+					React and Next.js with TypeScript, Java, Spring Boot, MySQL.
+				</span>{' '}
+				I am also familiar with Dart & Flutter. I am always looking to learn new
+				technologies. I am currently looking for a{' '}
 				<span className="font-medium">full-time position</span> as a software
-				developer.
+				developer, webdeveloper or mobile developer. I am intrested in both
+				remote and on-site work. I am currently based in Umeå, Sweden.
 			</p>
 			<p>
 				<span className="italic">When I'm not coding</span>, I enjoy playing
 				video games, watching movies, and playing with my dog. I also enjoy{' '}
 				<span className="font-medium">learning new things</span>. I am currently
-				learning about{' '}
-				<span className="font-medium">history and philosophy</span>. I'm also
-				learning how to play the guitar.
+				watch to many youtube videos about{' '}
+				<span className="font-medium">Space and Physics.</span>
 			</p>
 		</motion.section>
 	);
